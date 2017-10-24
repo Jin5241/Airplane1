c79f2cb243eaf33a5ba23e32ba38de69722b4a89
Jin5241 <gin.huangj@gmail.com>
2017-10-23 20:10:54 -0400

    ResultSort

--- src/DB/ReserveFlight.java
+++ src/DB/ReserveFlight.java
@@ -6,8 +6,14 @@ public class ReserveFlight {
     String resetDB="http://cs509.cs.wpi.edu:8181/CS509.server/ReservationSystem?team=404&action=resetDB";
     String ReserveFlight="http://cs509.cs.wpi.edu:8181/CS509.server/ReservationSystem?team=404&action=buyTickets&flightData"+xmlFlights;
 
-    public boolean lock(){
+    public static void lock(){
 
-        return
+
+    }
+    public static void reserve(){
+
+    }
+    public static void unlock(){
+
     }
 }
